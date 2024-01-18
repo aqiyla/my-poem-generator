a5e22e380b6ad22b91022180a1561bd2b7ddf740
aqiyla <144479918+aqiyla@users.noreply.github.com>
2024-01-18 12:19:20 -0500

    Improved the User Experience

--- src/index.js
+++ src/index.js
@@ -14,9 +14,13 @@ function generatePoem(event) {
   let instructionsInput = document.querySelector("#user-instructions");
   let apiKey = "7cc6e139312be6o1cb19t94fd0aeff4a";
   let context =
-    "You are a rhythmic Poem expert and love to write short poems. Your mission is to generate a 4 line poem in basic HTML and seperate each line with a <br />. Make sure to follow the user instructions below. Do not include a title to the poem and do not write the font in different colors.";
+    "You are a rhythmic Poem expert and love to write short poems. Your mission is to generate a 4 line poem in basic HTML and seperate each line with a <br />. No <br /> after the last line of the poem. Make sure to follow the user instructions below. Do not include a title to the poem and do not write the font in different colors.";
   let prompt = `User instructions: Generate a Rap poem about ${instructionsInput.value}`;
   let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
+
+  let poemElement = document.querySelector("#poem");
+  poemElement.classList.remove("hidden");
+  poemElement.innerHTML = `<div class="generating">⏳ Generating a Rap poem about ${instructionsInput.value}</div>`;
 
   console.log("Generating poem");
   console.log(`Prompt: ${prompt}`);
